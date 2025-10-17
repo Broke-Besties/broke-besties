c9858eb602a45c395399718618d2fc0d2161dc30
Daniel Venistan <danielvenistan@gmail.comm>
2025-10-17 18:48:13 -0400

    feat: add Clerk middleware and Prisma client integration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
 });
+
+app.use(clerkMiddleware());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
