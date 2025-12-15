e7fdf88ec28310859ea0288d6dd0a8cb14e25fb7
Albert Huynh <albert.huynh@ibm.com>
2025-12-14 20:28:02 -0500

    add the agent

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"), // If running a migration, change to env("DIRECT_URL")
+    url: env("DIRECT_URL"), // If running a migration, change to env("DIRECT_URL")
   },
 });
